0e5a8e0b2ab09115dbe4352bc94dfe11d987ebd5
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-06 15:07:18 +0200

    Add tests

--- app/src/androidTest/java/com/marcinmoskala/cde_droidcon_it/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/marcinmoskala/cde_droidcon_it/ExampleInstrumentedTest.java
@@ -2,25 +2,27 @@ package com.marcinmoskala.cde_droidcon_it;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule public ActivityTestRule<MainActivity> rule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.marcinmoskala.cde_droidcon_it", appContext.getPackageName());
+        Espresso.onView(ViewMatchers.withId(R.id.someId)).check(ViewAssertions.matches(ViewMatchers.withText("Hello to myself :)")));
     }
 }
